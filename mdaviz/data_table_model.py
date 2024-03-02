1209a624b08ba4675abb52eea77be651f00e5879
Fanny Rodolakis <rodolakis@anl.gov>
2024-03-01 18:17:48 -0600

    #81 build data table model and table view

--- mdaviz/data_table_model.py
+++ mdaviz/data_table_model.py
@@ -1,49 +1,57 @@
+from PyQt5 import QtCore
 from PyQt5.QtCore import QAbstractTableModel, QVariant, Qt
+from . import utils
 
 
 class DataTableModel(QAbstractTableModel):
 
-    def __init__(self, data, parent=None):
+    def __init__(self, detsDict, parent=None):
         """
-        Create the model and connect with its parent.
+                Create the model and connect with its parent.
 
-        Here the number of row is going to be the indexes, 
-        with the number of rows being the number of points 
-        in the selected scan
+                Here the number of row is going to be the indexes,
+                with the number of rows being the number of points
+                in the selected scan
 
-        The number of column will either:
-            - (1) all the data: all pos + all dets
-            - or (2) just the number of element 
-        in selection, eg: selection = {Y:[2,3],X=[0]},
-        then number of column will be 3
-        not taking into account Mon and Norm for now.
-        Do we want to always show indexes? 
-        For now (1) is easier, let's just do that.
+                The number of column will either:
+                    - (1) all the data: all pos + all dets
+                    - or (2) just the number of element
+                in selection, eg: selection = {Y:[2,3],X=[0]},
+                then number of column will be 3
+                not taking into account Mon and Norm for now.
+                Do we want to always show indexes?
+                For now (1) is easier, let's just do that.
 
-,
+        ,
 
-        PARAMETERS
-        headers:
-            will be defined by the selection from fields TV: 
-            pos and det(s)
-        parent object:
-            I don't know yet! I'd like it to be mda_folder
+                PARAMETERS
+                headers:
+                    will be defined by the selection from fields TV:
+                    pos and det(s)
+                parent object:
+                    I don't know yet! I'd like it to be MDA_MVC but might end up being FTV
         """
-        self.parent = parent  #TODO: rename when I know who's the parent
+        # TODO: confirm who's the parent: FTV or MDA_MVC?
+        self.mda_mvc = parent
         super().__init__(parent)
-        self.columnLabels = headers
+
+        self.setAllData()
+        self.setColumnLabels()
 
     def rowCount(self, parent=None):
-        value = len(self.data())
+        value = len(self.allData()[0]) if len(self.allData()) > 0 else 0
         return value
 
     def columnCount(self, parent=None):
-        # Number of columns is determined by the number of headers
-        return len(self.columnLabels)
+        # Number of columns is determined by the number of pos(s) & det(s)
+        return len(self.columnLabels())
 
     def data(self, index, role):
-        # No data is provided
-        return QVariant()
+        # display data
+        if role == QtCore.Qt.DisplayRole and self.setAllData is not {}:
+            label = self.columnLabels[index.column()]
+            value = self.allData()[label][index.row()]
+            return value
 
     def headerData(self, section, orientation, role):
         # Provide header labels
@@ -51,24 +59,30 @@ class DataTableModel(QAbstractTableModel):
             return self.columnLabels[section]
         return QVariant()
 
-    # # ------------ local methods
-
-    # def get_file_data(self, file, selection):
-    #     """Here dataList is going to be something like:
-    #     X (eg P1) = [1,2,3,4,5,6,7,8,9]
-    #     Y1 (eg D01) = [corresponding value for this detector]
-    #     Y2 (eg D08) = [corresponding value for this detector]
-    #     """       
-
+    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                return self.columnLabels[section]
+            else:
+                return str(section + 1)  # may want to alter at some point
 
     # # # ------------ get & set methods
 
-    def data(self):
-        """Here data is the content of a file?
-        """
-        return self._data
+    def columnLabels(self):
+        return self._columnLabels
 
-    def setData(self, data):
-        """Here arg data is?
-        """
-        self._data = data
+    def setColumnLabels(self):
+        self._columnLabels = list(self.allData.keys())
+        return self._columnLabels
+
+    def allData(self):
+        return self._allData
+
+    def setAllData(self, detsDict):
+        self._allData = {}
+        if detsDict:
+            for v in list(detsDict.values()):
+                pv = utils.byte2str(v.name)
+                data = v.data
+                self._allData[pv] = data
+        return self._allData
---
+++ mdaviz/data_table_view.py
@@ -0,0 +1,49 @@
+from PyQt5 import QtCore, QtWidgets
+from . import utils
+
+HEADERS = ["X", "Y"]
+
+
+class DataTableView(QtWidgets.QWidget):
+    ui_file = utils.getUiFileName(__file__)
+
+    def __init__(self, parent):
+        """
+        Create the table view and connect with its parent.
+
+        PARAMETERS
+
+        parent object:
+            Instance of mdaviz.mda_folder.MDAMVC
+        """
+
+        self.mda_mvc = parent
+        super().__init__()
+        utils.myLoadUi(self.ui_file, baseinstance=self)
+        self.setup()
+
+    def setup(self):
+        header = self.tableView.horizontalHeader()
+        header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
+
+    def displayTable(self):
+        from .data_table_model import DataTableModel
+        from .empty_table_model import EmptyTableModel
+
+        data = self.detsDict()
+        if len(data) > 0:
+            data_model = DataTableModel(data, self.mda_mvc)
+            self.tableView.setModel(data_model)
+        else:
+            empty_model = EmptyTableModel(HEADERS)
+            self.tableView.setModel(empty_model)
+
+    def detsDict(self):
+        return self.mda_mvc.select_fields_tableview.detsDict()
+
+    def setStatus(self, text):
+        self.mda_mvc.setStatus(text)
+
+    def clearContents(self):
+        # Clear the data model of the table view
+        self.tableView.setModel(None)
