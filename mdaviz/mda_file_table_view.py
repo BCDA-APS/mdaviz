d6bfe5743013ccc118a3b5fb5a82424974e55845
rodolakis <rodolakis@anl.gov>
2024-03-07 17:01:05 -0600

    ENH #90 rename detsDict => scanDict and to_dataset => subDatasets

--- mdaviz/mda_file_table_view.py
+++ mdaviz/mda_file_table_view.py
@@ -61,10 +61,10 @@ class MDAFileTableView(QtWidgets.QWidget):
 
     def data(self):
         """Return the data from the table view:
-              self.data= fileName, detsDict, fields
+              self.data= fileName, scanDict, fields
         with:
         - fileName (str): The name of the file without its extension.
-        - detsDict (dict): A dictionary of positioner & detector information.
+        - scanDict (dict): A dictionary of positioner & detector information.
         - field (list): List of TableField object, one for each det/pos:
                 ([TableField(name='P0', selection=None,...
                 ...desc='Index', pv='Index', unit='a.u'),...])
@@ -74,7 +74,7 @@ class MDAFileTableView(QtWidgets.QWidget):
     def setData(self):
         self._data = None
         if self.mda_file.data() is not None:
-            detsDict = self.mda_file.data()["detsDict"]
+            scanDict = self.mda_file.data()["scanDict"]
             fileName = self.mda_file.data()["fileName"]
             fields = [
                 TableField(
@@ -84,9 +84,9 @@ class MDAFileTableView(QtWidgets.QWidget):
                     desc=utils.byte2str(v.desc),
                     unit=utils.byte2str(v.unit),
                 )
-                for v in detsDict.values()
+                for v in scanDict.values()
             ]
-        self._data = {"fileName": fileName, "detsDict": detsDict, "fields": fields}
+        self._data = {"fileName": fileName, "scanDict": scanDict, "fields": fields}
 
     def displayTable(self):
         from .mda_file_table_model import MDAFileTableModel
@@ -113,54 +113,8 @@ class MDAFileTableView(QtWidgets.QWidget):
     def clearContents(self):
         self.tableView.setModel(None)
 
-    # ------ Creating datasets for the selected file:
 
-    # QUESTION: SHOULD THIS BE HERE OR IN MDA_FILE? WE NEED TO KEEP TRACK OF
-    # THE FILE PATH (BETTER THAN INDEX, 2 DIFFERENT FILE IN DIFFERENT FOLDER CAN
-    # HAVE THE SAME INDEX). =====> MAKE DATASETS PART OF SELF.MDA_FILE.DATA()
 
-    def to_datasets(self, selections):
-        """Prepare datasets and options for plotting with Matplotlib."""
-        datasets = []
-        file_name = self.data()["fileName"]
-        detsDict = self.data()["detsDict"]
 
-        # x_axis is the row number
-        x_axis = selections.get("X")
-        x_data = detsDict[x_axis].data if x_axis is not None else None
-        x_units = (
-            utils.byte2str(detsDict[x_axis].unit) if x_axis is not None else "a.u."
-        )
-        x_name = (
-            utils.byte2str(detsDict[x_axis].name) + f" ({x_units})"
-            if x_axis is not None
-            else "Index"
-        )
-
-        # y_axis is the list of row numbers
-        y_names_with_units = []
-        y_names_with_file_units = []
-        for y_axis in selections.get("Y", []):
-            y = detsDict[y_axis]
-            y_data = y.data
-            # y labels:
-            y_units = utils.byte2str(y.unit) if y.unit else "a.u."
-            y_name = utils.byte2str(y.name)
-            y_name_with_units = y_name + "  (" + y_units + ")"
-            y_name_with_file_units = file_name + ": " + y_name + "  (" + y_units + ")"
-            y_names_with_units.append(y_name_with_units)
-            y_names_with_file_units.append(y_name_with_file_units)
-            # append to dataset:
-            ds, ds_options = [], {}
-            ds_options["label"] = y_name_with_file_units
-            ds = [x_data, y_data] if x_data is not None else [y_data]
-            datasets.append((ds, ds_options))
-
-        plot_options = {
-            "x": x_name,  # label for x axis
-            "x_units": x_units,
-            "y": ", ".join(y_names_with_units[0:1]),  # label for y axis
-            "y_units": y_units,
-            "title": "",
-        }
-        return datasets, plot_options
+    def subDatasets(self,selections):
+        pass
