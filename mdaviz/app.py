5fdf16bde7541ee620a486a20f18961671b6accd
Pete Jemian <prjemian@gmail.com>
2024-05-31 11:26:38 -0500

    add options for logging and version

--- mdaviz/app.py
+++ mdaviz/app.py
@@ -4,6 +4,7 @@
 mdaviz: Python Qt5 application to visualize Bluesky data from tiled server.
 """
 
+import logging
 import sys
 
 
@@ -20,7 +21,44 @@ def gui():
     sys.exit(app.exec())
 
 
+def command_line_interface():
+    import argparse
+
+    from . import __version__
+
+    doc = __doc__.strip().splitlines()[0]
+    parser = argparse.ArgumentParser(description=doc)
+
+    # fmt: off
+    parser.add_argument(
+        "--log",
+        default="warning",
+        help=(
+            "Provide logging level. "
+            "Example '--log debug'. "
+            "Default level: 'warning'"),
+        choices=[k.lower() for k in logging.getLevelNamesMapping()],
+    )
+    # fmt: on
+
+    parser.add_argument("-v", "--version", action="version", version=__version__)
+
+    return parser.parse_args()
+
+
 def main():  # for future command-line options
+    global logger
+
+    options = command_line_interface()
+
+    logging.basicConfig(level=options.log.upper())
+    logger = logging.getLogger(__name__)
+    logger.info("Logging level: %s", options.log)
+
+    # set warning log level for (noisy) support packages
+    for package in "httpcore httpx PyQt5 tiled".split():
+        logging.getLogger(package).setLevel(logging.WARNING)
+
     gui()
 
 
