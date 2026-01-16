7c414570bbbcd32e6837eba1507f1c42831bd0bc
Fanny Rodolakis <rodolakis@anl.gov>
2026-01-16 14:38:03 -0600

    Run pre-commit

--- src/mdaviz/mainwindow.py
+++ src/mdaviz/mainwindow.py
@@ -618,9 +618,7 @@ class MainWindow(QMainWindow):
                             if self.mvc_folder and hasattr(
                                 self.mvc_folder, "mda_folder_tableview"
                             ):
-                                model = (
-                                    self.mvc_folder.mda_folder_tableview.tableView.model()
-                                )
+                                model = self.mvc_folder.mda_folder_tableview.tableView.model()
                                 if model and selected_index < model.rowCount():
                                     index = model.index(selected_index, 0)
                                     self.mvc_folder.selectAndShowIndex(index)
