3ecf33e68944033f33b102dd729399a4b0c910f1
Fanny Rodolakis <rodolakis@anl.gov>
2026-01-16 13:26:48 -0600

    Update docstring

--- src/mdaviz/mainwindow.py
+++ src/mdaviz/mainwindow.py
@@ -35,7 +35,6 @@ class MainWindow(QMainWindow):
 
     .. autosummary::
 
-        ~connect
         ~status
         ~setStatus
         ~doAboutDialog
@@ -53,8 +52,11 @@ class MainWindow(QMainWindow):
         ~setFolderList
         ~onFolderSelected
         ~onRefresh
-        ~_buildFolderList
-        ~_updateRecentFolders
+        ~doPreferences
+        ~doPopUp
+        ~proceed
+        ~cancel
+        ~get_auto_load_setting
     """
 
     # ==========================================
@@ -616,7 +618,9 @@ class MainWindow(QMainWindow):
                             if self.mvc_folder and hasattr(
                                 self.mvc_folder, "mda_folder_tableview"
                             ):
-                                model = self.mvc_folder.mda_folder_tableview.tableView.model()
+                                model = (
+                                    self.mvc_folder.mda_folder_tableview.tableView.model()
+                                )
                                 if model and selected_index < model.rowCount():
                                     index = model.index(selected_index, 0)
                                     self.mvc_folder.selectAndShowIndex(index)
